d6b657de7b725dd92a34636a25f99062c8c932cd
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-04-11 20:07:00 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,45 +2,10 @@ const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 import throttle from 'lodash.throttle';
 
-player.on('play', time);
+player.on('play', _.throttle(time, 1000));
 function time() {
   player.getCurrentTime().then(function (seconds) {
     localStorage.setItem('videoplayer-current-time', seconds);
   });
 }
-
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-
-// _.throttle(() => {
-//   player.on('play', time);
-//   function time() {
-//     player.getCurrentTime().then(function (seconds) {
-//       localStorage.setItem('videoplayer-current-time', seconds);
-//     });
-//   }
-// }, 1000)
-
-// player.on('play', time);
-// function time() {
-//   player.getCurrentTime().then(function (seconds) {
-//     _.throttle(() => {
-//       localStorage.setItem('videoplayer-current-time', seconds)
-//         }, 1000)
-//   });
-// }
-
-//   player.on('play', time);
-// function time() {
-//  _.throttle(() => {
-//   player.getCurrentTime().then(function (seconds) {
-//     localStorage.setItem('videoplayer-current-time', seconds);
-//   });
-//     }, 1000)
-// }
-
-// player.on('play', time);
-// function time() {
-//   player.getCurrentTime().then(function (seconds) {
-//     localStorage.setItem('videoplayer-current-time', seconds);
-//   });
-// }

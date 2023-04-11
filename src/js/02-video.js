6e8900e2c3e91283b5881785b02a05990a9954ca
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-04-11 14:51:48 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,7 +5,9 @@ import throttle from 'lodash.throttle';
 player.on('play', time);
 function time() {
   player.getCurrentTime().then(function (seconds) {
-    localStorage.setItem('videoplayer-current-time', seconds);
+    throttle(() => {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    }, 1000);
   });
 }
 
@@ -13,4 +15,4 @@ player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
 // test dzialania lodash
 const lodashTest = _.add(2, 3);
-console.log(lodashTest); // 
+console.log(lodashTest); //

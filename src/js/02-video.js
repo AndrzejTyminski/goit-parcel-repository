2a93fa585973085ee55b06e28b39d1abc5980496
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-04-11 15:01:25 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,11 @@
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle'
 
 player.on('play', time);
 function time() {
   player.getCurrentTime().then(function (seconds) {
-    throttle(() => {
-      localStorage.setItem('videoplayer-current-time', seconds);
-    }, 1000);
+    localStorage.setItem('videoplayer-current-time', seconds);
   });
 }
 
@@ -15,4 +13,4 @@ player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 
 // test dzialania lodash
 const lodashTest = _.add(2, 3);
-console.log(lodashTest); //
+console.log(lodashTest); // 

a06faaa038dd254162c6d1fd0f4fa97327bde331
AndrzejTyminski <117731002+AndrzejTyminski@users.noreply.github.com>
2023-04-11 12:48:53 +0000

    Deploying to gh-pages from @ AndrzejTyminski/goit-parcel-repository@dcf46abe79302c7f6a855d7028e0888b6506c5ef 🚀

--- 02-video.5489fb36.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){t.getCurrentTime().then((function(e){localStorage.setItem("videoplayer-current-time",e)}))})),t.setCurrentTime(localStorage.getItem("videoplayer-current-time"));var r=_.add(2,3);console.log(r)}();
-//# sourceMappingURL=02-video.5489fb36.js.map
--- 02-video.b2dc8d7c.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo.Player(e);t.on("play",(function(){t.getCurrentTime().then((function(e){localStorage.setItem("videoplayer-current-time",e)}))})),t.setCurrentTime(localStorage.getItem("videoplayer-current-time"));const o=_.add(2,3);console.log(o);
-//# sourceMappingURL=02-video.b2dc8d7c.js.map
